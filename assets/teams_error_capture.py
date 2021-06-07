3656ce4e887b02156862e31b76c117c031f769c1
Chris Metcalf <christopher.metcalf@fil.com>
2021-06-07 15:22:33 +0100

    Use LDAP accounts for Concourse instead of local auth
    We've removed all local auth from our Conci now

--- assets/teams_error_capture.py
+++ assets/teams_error_capture.py
@@ -1,4 +1,79 @@
-import json, requests, sseclient, sys, urllib3
+import json, requests, sseclient, sys, urllib3, re, tempfile
+
+from bs4 import BeautifulSoup
+from pathlib import Path
+from http import cookiejar
+
+proxies = {"http": "", "https": ""}
+
+def extract_auth_token(cookiejar, response):
+    """extract auth token from local cookie file"""
+    auth_token = None
+    concourse_cookies = ["skymarshal_auth0", "skymarshal_auth"]
+    for concourse_cookie in concourse_cookies:
+        for cookie in cookiejar:
+            if cookie.name == concourse_cookie:
+                auth_token = cookie.value.strip('"').split(' ')[1]
+                break
+    if not auth_token:
+        raise Exception(
+            "\x1b[31m Could not retrieve token {} from cookie\x1b[0m\ncurrent cookies: {}".format(
+                " or ".join(concourse_cookies), cookiejar
+            )
+        )
+    # Concourse truncated any cookies larger than 4000 bytes down to exactly 4000. Once you strip off the 'bearer ' part off the cookie, that leaves 3993 of truncated token.
+    # As the truncated cookie does not work, we now try a fallback technique to get the cookie directly from the HTML response that concourse sends.
+    # The response contains a <script> tag which includes the authToken, so we use BeautifulSoup and some regex to extract the token instead.
+    if len(auth_token) >= 3993:
+        soup = BeautifulSoup(response.content, features="html.parser")
+        scripts=soup.find_all('script')
+        for script in scripts:
+            if script.get('src') == None:
+                script_contents=script.string
+                auth_token=re.search(r'(authToken:\ \"bearer\ )(.*)\"', script_contents, re.IGNORECASE).group(2)
+    return auth_token
+
+
+def get_auth_paths(login_html):
+    """Parse the concourse login page html and return any auth paths (either be 2 paths LDAP+Local, or just local)"""
+    soup = BeautifulSoup(login_html, features="html.parser")
+    auth_paths = []
+    if soup.find('form'):
+        auth_paths.append(soup.find('form').get('action'))
+    else:
+        form_rows = soup.find_all(class_="theme-form-row")
+        for form_row in form_rows:
+            if form_row.find('a')['href']:
+                auth_paths.append(form_row.find('a')['href'])
+    return(auth_paths)
+
+def get_auth_token(api_target, concourse_username, concourse_password):
+    cookie_file = tempfile.NamedTemporaryFile()
+    cj = cookiejar.LWPCookieJar(cookie_file.name)
+    s = requests.Session()
+    s.cookies = cj
+    login_html = s.get(api_target+"/login", proxies=proxies, verify=False)
+    if login_html.status_code != 200:
+        print("Could not get login page from concourse.. Bailing")
+        sys.exit(1)
+    auth_paths = get_auth_paths(login_html.content)
+    # If available, ldap auth will be first item in the list, and if not it'll be local auth
+    if 'ldap' in auth_paths[0]:
+        username = concourse_username
+        password = concourse_password
+    else:
+        print("Concourse is not using LDAP authentication, aborting ...")
+        sys.exit(1)
+    auth_url = "{}/{}".format(api_target, auth_paths[0].lstrip('/'))
+    auth_payload = {
+        'login': username,
+        'password': password
+    }
+    r = s.post(auth_url, data=auth_payload, verify=False)
+    if 'csrf_token' not in r.url:
+        print("Failed to do concourse authentication... Aborting")
+        sys.exit(1)
+    return extract_auth_token(cj, r)
 
 def search_ids_names(current_id, accum_dict, plan):
     if 'id' in plan:
@@ -25,23 +100,7 @@ def main():
     build_number = sys.argv[4]
     log_max_length = int(sys.argv[5])
 
-    # Login
-    # OAuth2 token request
-    url = '{0}/sky/issuer/token'.format(concourse_hostname)
-    payload = {
-      'grant_type': 'password',
-      'username': concourse_username,
-      'password': concourse_password,
-      'scope': 'openid profile email federated:id groups'
-    }
-
-    response = requests.post(url, verify=False, data=payload, auth=('fly', 'Zmx5'))
-
-    if response.status_code != 200:
-        print(("Login failed (status: {0}). Exiting.").format(response.status_code))
-        sys.exit(1)
-
-    token = json.loads(response.content)['access_token']
+    token = get_auth_token(concourse_hostname, concourse_username, concourse_password)
 
     # Resolve taskid's to names
     url = '{0}/api/v1/builds/{1}/plan'.format(concourse_hostname, build_number)
